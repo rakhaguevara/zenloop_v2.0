514028582fa702a3d70b4624d5d1799f754de80c
Guevara <rakha.guevara2505@gmail.com>
2025-06-24 22:26:36 +0700

    "commit gambar"

--- version0.0/src/controller/LoginControllerCalmify.java
+++ version0.0/src/controller/LoginControllerCalmify.java
@@ -1,7 +1,6 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class LoginControllerCalmify {
@@ -10,11 +9,7 @@ public class LoginControllerCalmify {
     private ImageView leftImage;
 
     @FXML
-    private ImageView rightImage;
-
-    @FXML
     public void initialize() {
-
+        // Optional: bisa isi kode inisialisasi di sini
     }
-
 }
