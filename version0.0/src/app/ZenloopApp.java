919d29251a4aeaa00e499c4d9736c50a92b97752
GuevaraPC <rakha.guevara2505@gmail.com>
2025-07-20 20:16:24 +0700

    "update logo"

--- version0.0/src/app/ZenloopApp.java
+++ version0.0/src/app/ZenloopApp.java
@@ -1,10 +1,13 @@
 package app;
 
 import java.io.IOException;
+import java.util.Objects;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ZenloopApp extends Application {
@@ -12,27 +15,16 @@ public class ZenloopApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        // FXMLLoader fxmlLoader = new
-        // FXMLLoader(getClass().getResource("/version0.0/src/view/aiView.fxml"));
-        // FXMLLoader fxmlLoader = new
-        // FXMLLoader(getClass().getResource("/view/DashboardZen/journalArchive.fxml"));
-
-        // Parent root =
-        // FXMLLoader.load(getClass().getResource("/view/DashboardZen/stressStatistic.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Banner/banner.fxml"));
         Parent root = fxmlLoader.load();
-
-        // FXMLLoader fxmlLoader = new
-        // FXMLLoader(getClass().getResource("/view/DashboardZen/journalArchive.fxml"));
-        // Parent root = fxmlLoader.load();
-
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/view/style.css").toExternalForm());
         scene.getStylesheets().add(getClass().getResource("/view/card-journal.css").toExternalForm());
 
-        stage.getIcons()
-                .add(new javafx.scene.image.Image(getClass().getResourceAsStream("/app/resource/zenloopLogo.png")));
+        Image logo = new Image(Objects.requireNonNull(
+                getClass().getResourceAsStream("/app/resource/zenloopLogo.png")));
 
+        stage.getIcons().add(logo);
         stage.setScene(scene);
         stage.setResizable(false);
 
